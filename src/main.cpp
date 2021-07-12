a9ddab9f1e16bb914a30f34b8cdd10874f7d0d28
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-07-12 09:16:48 -0400

    added command line options

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include "ApolloRaytracer.hpp"
 
+#include <cxxopts.hpp>
+
 int main(int argc, char *argv[]) {
-    ApolloRaytracer apollo("assets/scenes/teapots.json", 400, 400, 1, 3);
+
+    cxxopts::Options options("Apollo Raytracer",
+                             "A hobby raytracer written in C++");
+
+    // clang-format off
+
+    options.add_options()
+    ("s,scene", "specify scene file to load", cxxopts::value<std::string>()->default_value("assets/scenes/testScene.json"))
+    ("w,width", "specify output width", cxxopts::value<int>()->default_value("400"))
+    ("h,height", "specify output height", cxxopts::value<int>()->default_value("400"))
+    ("r,res", "specify number of pixels per ray", cxxopts::value<int>()->default_value("1"))
+    ("d,depth", "specify maximum ray depth", cxxopts::value<int>()->default_value("3"))
+    ("help", "prints help message");   
+    ;
+
+    // clang-format on
+
+    auto results = options.parse(argc, argv);
+
+    if (results.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    std::string scene = results["scene"].as<std::string>();
+    int width = results["width"].as<int>();
+    int height = results["height"].as<int>();
+    int res = results["res"].as<int>();
+    int depth = results["depth"].as<int>();
+
+    ApolloRaytracer apollo(scene, width, height, res, depth);
     apollo.run();
     return 0;
 }
